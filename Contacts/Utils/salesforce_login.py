2e43d1285b0880460454257ce83a5027511eac88
ufcSaadKhan <saad.khan@unitedforcare.com.au>
2025-05-27 11:26:56 +1000

    Update salesforce_login.py

--- Contacts/Utils/salesforce_login.py
+++ Contacts/Utils/salesforce_login.py
@@ -2,7 +2,5 @@
 from simple_salesforce import Salesforce
 
 sf = Salesforce(
-    username='saad.khan@unitedforcare.com.au',
-    password='Soccerate6.',
-    security_token='mn0gCHdyefZAj5ckF4US29kX'
+
 )
